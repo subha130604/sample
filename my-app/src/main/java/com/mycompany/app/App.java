14055fa6cde43b43ebdae2f9a343dbe043bc7912
subha130604 <146178502+subha130604@users.noreply.github.com>
2024-04-09 19:39:41 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -16,6 +16,10 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+     if (args.length == 0) {
+            System.out.println("Please provide a number as a command-line argument.");
+            return;
+        }
 
         Scanner reader = new Scanner(System.in);
 
